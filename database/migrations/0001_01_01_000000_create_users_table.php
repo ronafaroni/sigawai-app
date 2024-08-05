d5b5c9eab75918bbb3663e9bc857be252aed3e34
ronafaroni <ronafaroni95@gmail.com>
2024-08-05 09:53:21 +0700

    mengganti migration di tabel users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('niy', 20)->unique();
+            $table->string('nama_pegawai');
+            $table->string('username');
             $table->string('password');
             $table->string('role');
             $table->rememberToken();
